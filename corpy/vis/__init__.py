eeaf36b73ffa192d746b1e14f561a2e3861717e1
David Lukes <dafydd.lukes@gmail.com>
2018-11-16 16:10:11 +0100

    Improve wordcloud visualization
    More thorough implementation, more features (e.g. elliptical mask
    toggle), better documentation, tests.

--- corpy/vis/__init__.py
+++ corpy/vis/__init__.py
@@ -1,5 +1,7 @@
+from collections import Counter
 from collections.abc import Mapping, Iterable
 
+import numpy as np
 from wordcloud import WordCloud
 
 CM_PER_IN = 2.54
@@ -25,32 +27,13 @@ def size_in_pixels(width, height, unit="in", ppi=300):
     if unit not in allowed_units:
         raise ValueError(f"`unit` must be one of {allowed_units}.")
     if unit == "cm":
-        width *= CM_PER_IN
-        height *= CM_PER_IN
+        width = round(width * CM_PER_IN)
+        height = round(height * CM_PER_IN)
     return width * ppi, height * ppi
 
 
-def wordcloud(data, size=(400, 400), *, fast=True, shape=None, **kwargs):
-    """Generate a wordcloud.
-
-    data: input data -- either one long string of text, or an
-        iterable of tokens, or a mapping of word types to their
-        frequencies; use the third option if you want full control
-        over the input data
-    size: size in pixels, as a tuple of integers, (width, height);
-        if you want to specify the size in inches or cm, use the
-        `size_in_pixels()` function to generate this tuple
-    fast: when True, optimizes large wordclouds for speed of
-        generation rather than precision of word placement
-    kwargs: remaining keyword arguments are passed on to the
-        `wordcloud.WordCloud` initializer
-
-    """
-    # TODO: implement shapes (circle, ellipse)
-    if shape is not None:
-        raise NotImplementedError("Specifying shapes has not been implemented yet.")
+def _optimize_dimensions(size, fast, fast_limit):
     width, height = size
-    fast_limit = 800
     # NOTE: Reasonable numbers for width and height are in the hundreds
     # to low thousands of pixels. If the requested size is large, for
     # faster results, we shrink the canvas during wordcloud
@@ -61,6 +44,52 @@ def wordcloud(data, size=(400, 400), *, fast=True, shape=None, **kwargs):
         height = round(height / scale)
     else:
         scale = 1
+    return width, height, scale
+
+
+def _elliptical_mask(width, height):
+    x_center = half_width = round(width / 2)
+    y_center = half_height = round(height / 2)
+    x = np.arange(0, width)
+    y = np.arange(0, height)[:, None]
+    mat = ((x - x_center) / half_width) ** 2 + ((y - y_center) / half_height) ** 2
+    return (mat >= 1) * 255
+
+
+def wordcloud(
+    data, size=(400, 400), *, rounded=False, fast=True, fast_limit=800, **kwargs
+):
+    """Generate a wordcloud.
+
+    If `data` is a string, the wordcloud is generated using the
+    method `.generate_from_text()`, which automatically ignores
+    stopwords (customizable with the `stopwords` argument) and
+    includes "collocations" (i.e. bigrams).
+
+    If `data` is a sequence or a mapping, the wordcloud is generated
+    using the method `.generate_from_frequencies()` and these
+    preprocessing responsibilities fall to the user.
+
+    data: input data -- either one long string of text, or an
+        iterable of tokens, or a mapping of word types to their
+        frequencies; use the second or third option if you want
+        full control over the output
+    size: size in pixels, as a tuple of integers, (width, height);
+        if you want to specify the size in inches or cm, use the
+        `size_in_pixels()` function to generate this tuple
+    rounded: whether or not to enclose the wordcloud in an ellipse;
+        incompatible with the `mask` keyword argument
+    fast: when True, optimizes large wordclouds for speed of
+        generation rather than precision of word placement
+    fast_limit: speed optimizations for "large" wordclouds are
+        applied when the requested canvas size is larger than
+        `fast_limit**2`
+    kwargs: remaining keyword arguments are passed on to the
+        `wordcloud.WordCloud` initializer
+
+    """
+    if rounded and kwargs.get("mask"):
+        raise ValueError("Can't specify `rounded` and `mask` at the same time.")
     # tweak defaults
     kwargs.setdefault("background_color", "white")
     # if Jupyter gets better at rendering transparent images, then
@@ -68,7 +97,11 @@ def wordcloud(data, size=(400, 400), *, fast=True, shape=None, **kwargs):
     # kwargs.setdefault("mode", "RGBA")
     # kwargs.setdefault("background_color", None)
 
+    width, height, scale = _optimize_dimensions(size, fast, fast_limit)
+    if rounded:
+        kwargs["mask"] = _elliptical_mask(width, height)
     wc = WordCloud(width=width, height=height, scale=scale, **kwargs)
+
     # raw text
     if isinstance(data, str):
         return wc.generate_from_text(data)
@@ -82,7 +115,7 @@ def wordcloud(data, size=(400, 400), *, fast=True, shape=None, **kwargs):
     # base classes don't pick up on it (maybe because it only has a
     # __getitem__ magic method?)
     elif isinstance(data, Iterable) or hasattr(data, "__getitem__"):
-        return wc.generate_from_text(" ".join(data))
+        return wc.generate_from_frequencies(Counter(data))
     else:
         raise ValueError(
             "`data` must be a string, a mapping from words to frequencies, or an iterable of words."
---
+++ tests/test_wordcloud.py
@@ -0,0 +1,116 @@
+import pytest
+import numpy as np
+
+from corpy import vis
+
+
+def test_size_in_pixels():
+    assert vis.size_in_pixels(5, 6) == (1500, 1800)
+    assert vis.size_in_pixels(5, 6, ppi=96) == (480, 576)
+    assert vis.size_in_pixels(5, 6, unit="cm") == (3900, 4500)
+    assert vis.size_in_pixels(4, 10, unit="cm", ppi=96) == (960, 2400)
+    with pytest.raises(ValueError) as exc_info:
+        vis.size_in_pixels(5, 5, unit="foo")
+    assert str(exc_info.value) == "`unit` must be one of ('in', 'cm')."
+
+
+def test__optimize_dimensions():
+    # canvas gets shrunk for computation with scale factor for output
+    # when fast == True...
+    assert vis._optimize_dimensions((10000, 10000), True, 800) == (800, 800, 12.5)
+    assert vis._optimize_dimensions((1000, 10000), True, 800) == (80, 800, 12.5)
+
+    # ... but is kept untouched when fast == False...
+    assert vis._optimize_dimensions((10000, 10000), False, 800) == (10000, 10000, 1)
+
+    # ... or when the size is at or below fast_limit**2
+    assert vis._optimize_dimensions((800, 800), True, 800) == (800, 800, 1)
+    assert vis._optimize_dimensions((640, 1000), True, 800) == (640, 1000, 1)
+    assert vis._optimize_dimensions((500, 500), True, 800) == (500, 500, 1)
+
+
+def test__elliptical_mask():
+    assert np.all(
+        vis._elliptical_mask(5, 5)
+        == np.array(
+            [
+                [255, 255, 255, 255, 255],
+                [255, 0, 0, 0, 255],
+                [255, 0, 0, 0, 255],
+                [255, 0, 0, 0, 255],
+                [255, 255, 255, 255, 255],
+            ]
+        )
+    )
+    assert np.all(
+        vis._elliptical_mask(10, 5)
+        == np.array(
+            [
+                [255, 255, 255, 255, 255, 255, 255, 255, 255, 255],
+                [255, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [255, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [255, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [255, 255, 255, 255, 255, 255, 255, 255, 255, 255],
+            ]
+        )
+    )
+    assert np.all(
+        vis._elliptical_mask(5, 10)
+        == np.array(
+            [
+                [255, 255, 255, 255, 255],
+                [255, 0, 0, 0, 255],
+                [255, 0, 0, 0, 255],
+                [255, 0, 0, 0, 255],
+                [255, 0, 0, 0, 255],
+                [255, 0, 0, 0, 255],
+                [255, 0, 0, 0, 255],
+                [255, 0, 0, 0, 255],
+                [255, 0, 0, 0, 255],
+                [255, 0, 0, 0, 255],
+            ]
+        )
+    )
+
+
+def test_wordcloud():
+    import os
+    from collections import Counter
+
+    # smaller than fast_limit**2
+    im = vis.wordcloud(os.__doc__).to_image()
+    assert (im.width, im.height) == (400, 400)
+    im = vis.wordcloud(os.__doc__, (400, 800)).to_image()
+    assert (im.width, im.height) == (400, 800)
+    im = vis.wordcloud(os.__doc__, (800, 400)).to_image()
+    assert (im.width, im.height) == (800, 400)
+
+    # larger than fast_limit**2
+    im = vis.wordcloud(os.__doc__, (813, 845)).to_image()
+    assert (im.width, im.height) == (813, 844)
+    im = vis.wordcloud(os.__doc__, (813, 845), fast=False).to_image()
+    assert (im.width, im.height) == (813, 845)
+
+    # smaller than custom fast_limit
+    im = vis.wordcloud(os.__doc__, (813, 845), fast_limit=829).to_image()
+    assert (im.width, im.height) == (813, 845)
+
+    # different types of input
+    assert vis.wordcloud(os.__doc__.split())
+    assert vis.wordcloud(w for w in os.__doc__.split())
+    assert vis.wordcloud(Counter(os.__doc__.split()))
+
+    # using an elliptical mask doesn't affect output size
+    im = vis.wordcloud(os.__doc__, (313, 345), rounded=True).to_image()
+    assert (im.width, im.height) == (313, 345)
+
+    # exceptions
+    with pytest.raises(ValueError) as exc_info:
+        vis.wordcloud(None)
+    assert (
+        str(exc_info.value)
+        == "`data` must be a string, a mapping from words to frequencies, or an iterable of words."
+    )
+    with pytest.raises(ValueError) as exc_info:
+        vis.wordcloud(os.__doc__, rounded=True, mask=1)
+    assert str(exc_info.value) == "Can't specify `rounded` and `mask` at the same time."
