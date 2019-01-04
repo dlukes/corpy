b10951b194fbac7869ff157b6e3b369f9b5c1ad8
David Lukes <dafydd.lukes@gmail.com>
2019-01-04 13:37:11 +0100

    Replace glimpse with head

--- corpy/util/__init__.py
+++ corpy/util/__init__.py
@@ -1,7 +1,37 @@
-def glimpse(obj, max_len=25):
-    """Get repr of object, truncated if too long."""
-    rep = repr(obj)
-    return rep[:20] + "..." + rep[-1] if len(rep) > max_len else rep
+from pprint import pprint
+
+
+def _head_gen(items, n):
+    for idx, item in enumerate(items):
+        if n == idx:
+            break
+        yield item
+
+
+def head(collection, n=None):
+    """Inspect collection, truncated if too long.
+
+    If n is None, an appropriate value is determined based on the type
+    of the collection.
+
+    """
+    type_ = type(collection)
+    if n is None:
+        if type_ in (str, bytes):
+            n = 100
+        else:
+            n = 10
+    if len(collection) <= n:
+        pprint(collection)
+        return
+    if type_ == str:
+        constructor = "".join
+    elif type_ == bytes:
+        constructor = b"".join
+    else:
+        constructor = type_
+    items = collection.items() if hasattr(collection, "items") else collection
+    pprint(constructor(_head_gen(items, n)))
 
 
 def cmp(lhs, rhs, test="__eq__"):
