449b6fb0ad6c2f60108f5b8bd23a306e74854d00
David Lukes <dafydd.lukes@gmail.com>
2019-05-24 13:18:38 +0200

    Improve linting and testing config

--- check.sh
+++ check.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 flake8
-# these are the same settings as enabled by Visual Studio Code, cf:
-# https://code.visualstudio.com/docs/python/linting
-pylint corpy --unsafe-load-any-extension=y --disable=all --enable=F,E,unreachable,duplicate-key,unnecessary-semicolon,global-variable-not-assigned,unused-variable,binary-op-exception,bad-format-string,anomalous-backslash-in-string,bad-open-mode
+pylint corpy
 pytest
 poetry build
 twine check dist/*
