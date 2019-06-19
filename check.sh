0b761a4cfff4cce596f14ba767ebc4b6a2245507
David Lukes <dafydd.lukes@gmail.com>
2019-06-19 19:12:08 +0200

    Fix grep exit code bug in check.sh

--- check.sh
+++ check.sh
@@ -28,8 +28,8 @@ pytest -n auto
 # interested in missing references to stuff defined as part of corpy, and not
 # the warning emitted upon importing corpy when no IPython session is found
 sphinx-build -j auto -Ean docs docs/_build 2>&1 |
-  rg "WARNING.*corpy" |
-  rg -v "IPython session not found"
+  { grep -P "WARNING.*corpy" || [[ $? == 1 ]] } |
+  { grep -Pv "IPython session not found" || [[ $? == 1 ]] }
 # possibly also check external links every now and then
 # sphinx-build -b linkcheck docs docs/_build
 rm -rf dist
