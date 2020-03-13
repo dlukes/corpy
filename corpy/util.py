5a26610be5df64f0155204b1b5f46526adedb47c
David Lukes <dafydd.lukes@gmail.com>
2020-03-13 17:32:28 +0100

    Add clean_env context manager

--- corpy/util.py
+++ corpy/util.py
@@ -2,6 +2,7 @@
 
 """
 from pprint import pprint
+from contextlib import contextmanager
 
 
 def _head_gen(items, first_n):
@@ -49,3 +50,49 @@ def cmp(lhs, rhs, test="__eq__"):
     if ans is NotImplemented:
         ans = lhs is rhs
     assert ans, msg
+
+
+@contextmanager
+def clean_env(
+    *,
+    blacklist=None,
+    whitelist=None,
+    keep_dunder=True,
+    keep_callables=True,
+    keep_sunder=False,
+):
+    """Run a block of code in a sanitized global environment.
+
+    This is useful e.g. for testing answers in student assignments,
+    because it will ensure that functions which accidentally capture
+    global variables instead of using arguments fail.
+
+    By default, global variables starting with a double underscore and
+    callables are kept. A whitelist of additional variables to keep can
+    also be provided. The environment is restored afterwards.
+
+    """
+    if blacklist and whitelist:
+        raise ValueError("Only one of blacklist or whitelist can be specified.")
+
+    hidden_globs = {}
+    if blacklist:
+        for name in blacklist:
+            value = globals().pop(name)
+            hidden_globs[name] = value
+    else:
+        for name, value in list(globals().items()):
+            is_dunder = name.startswith("__")
+            if not (
+                (keep_dunder and is_dunder)
+                or (keep_callables and callable(value))
+                or (keep_sunder and name.startswith("_") and not is_dunder)
+                or (whitelist and name in whitelist)
+            ):
+                del globals()[name]
+                hidden_globs[name] = value
+
+    try:
+        yield
+    finally:
+        globals().update(hidden_globs)
