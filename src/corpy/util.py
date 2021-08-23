79e28e13728e8c5db67993a6199ef82a4df00d65
David Lukes <dafydd.lukes@gmail.com>
2021-08-23 12:28:30 +0200

    Remove util.{head,cmp}
    head was motivated by NLTK suddenly spewing long lists of tokens instead of
    previews, as it'd been doing previously, but I never remember to use it anyway,
    and it's better to use the occasion to remind students about slicing. Also, it
    feels like NLTK has fixed this in some places, reverting to the old behavior
    (just a hunch based, I feel like I've been less frustrated with this issue over
    recent iterations of my course).
    
    As for cmp -- again, I never remember to use it, it's somewhat brittle (because
    it relies on head, among other things), and it's probably better to write
    asserts so that 1) students can learn about them, and 2) you can have custom
    behavior and messages more appropriate in the given context.
    
    Closes #7.

--- src/corpy/util.py
+++ src/corpy/util.py
@@ -4,69 +4,10 @@
 import sys
 import inspect
 import builtins
-from pprint import pprint
 from contextlib import contextmanager
 
 from types import FrameType, GeneratorType
 from typing import Optional, Iterable, Tuple
-
-
-#
-# ------------------------------------------------------------------------- Head {{{1
-
-
-def _head_gen(items, first_n):
-    for idx, item in enumerate(items):
-        if first_n == idx:
-            break
-        yield item
-
-
-def head(collection, first_n=None):
-    """Inspect `collection`, truncated if too long.
-
-    >>> head(list(range(1_000)))
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-    If ``first_n=None``, an appropriate value is determined based on the type
-    of the collection.
-
-    """
-    type_ = type(collection)
-    if first_n is None:
-        if type_ in (str, bytes):
-            first_n = 100
-        else:
-            first_n = 10
-    if len(collection) <= first_n:
-        pprint(collection)
-        return
-    if type_ == str:
-        constructor = "".join
-    elif type_ == bytes:
-        constructor = b"".join
-    else:
-        constructor = type_
-    items = collection.items() if hasattr(collection, "items") else collection
-    pprint(constructor(_head_gen(items, first_n)))
-
-
-#
-# -------------------------------------------------------------------------- Cmp {{{1
-
-
-def cmp(lhs, rhs, test="__eq__"):
-    """Wrap assert statement to automatically raise an informative error."""
-    msg = f"{head(lhs)} {test} {head(rhs)} is not True!"
-    ans = getattr(lhs, test)(rhs)
-    # operators automatically fall back to identity comparison if the
-    # comparison is not implemented for the given types, magic methods don't →
-    # if comparison is not implemented, we must fall back to identity
-    # comparison manually, because NotImplemented is truthy and makes the
-    # assert succeed
-    if ans is NotImplemented:
-        ans = lhs is rhs
-    assert ans, msg
 
 
 #
