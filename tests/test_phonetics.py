f5e53413664b1dd8b122400ae031a3ac2d5222a0
David Lukes <dafydd.lukes@gmail.com>
2021-09-07 16:34:26 +0200

    Refactor phonetics tests
    Multiple test cases for the same concept should use
    pytest.mark.parametrize, so that each test case is separate, instead of
    multiple asserts.
    
    Classes can be used to group related tests together.

--- tests/test_phonetics.py
+++ tests/test_phonetics.py
@@ -1,172 +1,262 @@
 import pytest
+from pathlib import Path
 
 from corpy.phonetics import cs
+from corpy.morphodita import Tagger
+
+SCRIPT_DIR = Path(__file__).parent
+TAGGER = Tagger(SCRIPT_DIR.parent / "czech-morfflex-pdt-161115.tagger")
 
 
 def test_voicing_assimilation_over_word_boundaries():
     assert cs.transcribe("máš hlad") == [("m", "a:", "Z"), ("h\\", "l", "a", "t")]
 
 
-def test_voicing_assimilation_of_ř():
-    assert cs.transcribe("pařát") == [("p", "a", "P\\", "a:", "t")]
-    assert cs.transcribe("přát") == [("p", "Q\\", "a:", "t")]
-    assert cs.transcribe("hřát") == [("h\\", "P\\", "a:", "t")]
+@pytest.mark.parametrize(
+    "orth,phon",
+    [
+        ("pařát", [("p", "a", "P\\", "a:", "t")]),
+        ("přát", [("p", "Q\\", "a:", "t")]),
+        ("hřát", [("h\\", "P\\", "a:", "t")]),
+    ],
+)
+def test_voicing_assimilation_of_ř(orth, phon):
+    assert cs.transcribe(orth) == phon
 
 
-def test_different_output_alphabets():
-    assert cs.transcribe("máš hlad", alphabet="IPA") == [
-        ("m", "aː", "ʒ"),
-        ("ɦ", "l", "a", "t"),
-    ]
-    assert cs.transcribe("máš hlad", alphabet="CS") == [
-        ("m", "á", "ž"),
-        ("h", "l", "a", "t"),
-    ]
-    assert cs.transcribe("máš hlad", alphabet="CNC") == [
-        ("m", "á", "ž"),
-        ("h", "l", "a", "t"),
-    ]
+@pytest.mark.parametrize(
+    "orth,alphabet,phon",
+    [
+        (
+            "máš hlad",
+            "IPA",
+            [
+                ("m", "aː", "ʒ"),
+                ("ɦ", "l", "a", "t"),
+            ],
+        ),
+        (
+            "máš hlad",
+            "CS",
+            [
+                ("m", "á", "ž"),
+                ("h", "l", "a", "t"),
+            ],
+        ),
+        (
+            "máš hlad",
+            "CNC",
+            [
+                ("m", "á", "ž"),
+                ("h", "l", "a", "t"),
+            ],
+        ),
+    ],
+)
+def test_different_output_alphabets(orth, alphabet, phon):
+    assert cs.transcribe(orth, alphabet=alphabet) == phon
 
 
-def test_hiatus():
-    # no hiatus by default
-    assert cs.transcribe("hiát") == [("h\\", "I", "a:", "t")]
-    # but can be requested optionally
-    assert cs.transcribe("hiát", hiatus=True) == [("h\\", "I", "j", "a:", "t")]
+class TestHiatus:
+    def test_no_hiatus_by_default_in_most_cases(self):
+        assert cs.transcribe("hiát") == [("h\\", "I", "a:", "t")]
+
+    def test_hiatus_by_default_between_high_front_vowels(self):
+        assert cs.transcribe("Indii") == [("I", "n", "d", "I", "j", "I")]
+
+    def test_hiatus_can_be_optionally_forced(self):
+        assert cs.transcribe("hiát", hiatus=True) == [("h\\", "I", "j", "a:", "t")]
 
 
-def test_other_csps():
-    assert cs.transcribe("tramvaj") == [("t", "r", "a", "F", "v", "a", "j")]
-    assert cs.transcribe("kongo") == [("k", "o", "N", "g", "o")]
+@pytest.mark.parametrize(
+    "orth,phon",
+    [
+        ("tramvaj", [("t", "r", "a", "F", "v", "a", "j")]),
+        ("kongo", [("k", "o", "N", "g", "o")]),
+    ],
+)
+def test_other_csps(orth, phon):
+    assert cs.transcribe(orth) == phon
 
 
-def test_remove_duplicate_graphemes():
-    assert cs.transcribe("denně") == [("d", "E", "J", "E")]
-    # short vowels are exempt for obvious reasons:
-    assert cs.transcribe("pootevřít") == [
-        ("p", "o", "o", "t", "E", "v", "P\\", "i:", "t")
-    ]
-    assert cs.transcribe("neexistoval") == [
-        ("n", "E", "E", "g", "z", "I", "s", "t", "o", "v", "a", "l")
-    ]
-    assert cs.transcribe("muzeem") == [("m", "u", "z", "E", "E", "m")]
-    # but long vowels are not
-    assert cs.transcribe("áá") == [("a:",)]
+@pytest.mark.parametrize(
+    "orth,phon",
+    [
+        ("denně", [("d", "E", "J", "E")]),
+        # short vowels are exempt for obvious reasons:
+        ("pootevřít", [("p", "o", "o", "t", "E", "v", "P\\", "i:", "t")]),
+        ("neexistoval", [("n", "E", "E", "g", "z", "I", "s", "t", "o", "v", "a", "l")]),
+        ("muzeem", [("m", "u", "z", "E", "E", "m")]),
+        # but long vowels are not
+        ("áá", [("a:",)]),
+    ],
+)
+def test_remove_duplicate_graphemes(orth, phon):
+    assert cs.transcribe(orth) == phon
 
 
-def test_no_gemination():
-    assert cs.transcribe("dražší") == [("d", "r", "a", "S", "i:")]
-    # but gemination currently remains allowed across word boundaries
-    assert cs.transcribe("t t") == [("t",), ("t",)]
+@pytest.mark.parametrize(
+    "orth,phon",
+    [
+        ("dražší", [("d", "r", "a", "S", "i:")]),
+        # but gemination currently remains allowed across word boundaries
+        ("t t", [("t",), ("t",)]),
+    ],
+)
+def test_no_gemination(orth, phon):
+    assert cs.transcribe(orth) == phon
 
 
-def test_exceptions():
+@pytest.mark.parametrize(
+    "orth,phon",
+    [
+        # partial replacements work as expected...
+        ("cyklistický", [("t_s", "I", "k", "l", "I", "s", "t", "I", "t_s", "k", "i:")]),
+        ("cyklisti", [("t_s", "I", "k", "l", "I", "s", "c", "I")]),
+        # ... even with a prefix
+        (
+            "necyklistický",
+            [("n", "E", "t_s", "I", "k", "l", "I", "s", "t", "I", "t_s", "k", "i:")],
+        ),
+        ("necyklisti", [("n", "E", "t_s", "I", "k", "l", "I", "s", "c", "I")]),
+        # the regex is properly constructed so that longer matches win: this
+        # should match komuni...
+        ("komunita", [("k", "o", "m", "u", "n", "I", "t", "a")]),
+        # ... this too...
+        ("komunisti", [("k", "o", "m", "u", "n", "I", "s", "c", "I")]),
+        # .. and this should match komunisti.
+        (
+            "komunistický",
+            [("k", "o", "m", "u", "n", "I", "s", "t", "I", "t_s", "k", "i:")],
+        ),
+        # contiguous matches starting at the beginning of the string can be
+        # rewritten...
+        (
+            "antikomunista",
+            [("a", "n", "t", "I", "k", "o", "m", "u", "n", "I", "s", "t", "a")],
+        ),
+        # ... but once the contiguity is broken (as here by the substring FOO),
+        # rewriting stops (notice -JIsta below instead of -nIsta)
+        (
+            "antiFOOkomunista",
+            [
+                (
+                    "a",
+                    "n",
+                    "t",
+                    "I",
+                    "f",
+                    "o",
+                    "o",
+                    "k",
+                    "o",
+                    "m",
+                    "u",
+                    "J",
+                    "I",
+                    "s",
+                    "t",
+                    "a",
+                )
+            ],
+        ),
+        # a no-op rule with a longer match trigger should make sure that this is
+        # not rewritten by the tip → typ rule...
+        ("tipec", [("c", "I", "p", "E", "t_s")]),
+        # ... while this is
+        ("tipovat", [("t", "I", "p", "o", "v", "a", "t")]),
+        # franco is rewritten...
+        ("franco", [("f", "r", "a", "N", "k", "o")]),
+        # ... but only when a full match
+        ("francouz", [("f", "r", "a", "n", "t_s", "o_u", "s")]),
+        # no voicing assimilation
+        ("tbilisi", [("t", "b", "I", "l", "I", "s", "I")]),
+        # no diphthong
+        ("používat", [("p", "o", "u", "Z", "i:", "v", "a", "t")]),
+        # no hiatus
+        ("využívat", [("v", "I", "u", "Z", "i:", "v", "a", "t")]),
+        # preventing deduplication
+        ("odděl", [("o", "d", "J\\", "E", "l")]),
+        ("odtáhni", [("o", "t", "t", "a:", "h\\", "J", "I")]),
+    ],
+)
+def test_exceptions(orth, phon):
     """NOTE: Use these tests as a source of inspiration as to what can be
-    achieved using exceptions. The system is kind of ugly and hacky but
-    thanks to that, it"s also flexible.
+    achieved using exceptions. The system is kind of ugly and hacky but thanks
+    to that, it's also flexible.
 
     """
-    # partial replacements work as expected...
-    assert cs.transcribe("cyklistický") == [
-        ("t_s", "I", "k", "l", "I", "s", "t", "I", "t_s", "k", "i:")
-    ]
-    assert cs.transcribe("cyklisti") == [("t_s", "I", "k", "l", "I", "s", "c", "I")]
-    # ... even with a prefix
-    assert cs.transcribe("necyklistický") == [
-        ("n", "E", "t_s", "I", "k", "l", "I", "s", "t", "I", "t_s", "k", "i:")
-    ]
-    assert cs.transcribe("necyklisti") == [
-        ("n", "E", "t_s", "I", "k", "l", "I", "s", "c", "I")
-    ]
-    # the regex is properly constructed so that longer matches win: this should
-    # match komuni...
-    assert cs.transcribe("komunita") == [("k", "o", "m", "u", "n", "I", "t", "a")]
-    # ... this too...
-    assert cs.transcribe("komunisti") == [("k", "o", "m", "u", "n", "I", "s", "c", "I")]
-    # .. and this should match komunisti.
-    assert cs.transcribe("komunistický") == [
-        ("k", "o", "m", "u", "n", "I", "s", "t", "I", "t_s", "k", "i:")
-    ]
-    # contiguous matches starting at the beginning of the string can be
-    # rewritten...
-    assert cs.transcribe("antikomunista") == [
-        ("a", "n", "t", "I", "k", "o", "m", "u", "n", "I", "s", "t", "a")
-    ]
-    # ... but once the contiguity is broken (as here by the substring FOO),
-    # rewriting stops (notice -JIsta below instead of -nIsta)
-    assert cs.transcribe("antiFOOkomunista") == [
-        ("a", "n", "t", "I", "f", "o", "o", "k", "o", "m", "u", "J", "I", "s", "t", "a")
-    ]
-    # a no-op rule with a longer match trigger should make sure that this is
-    # not rewritten by the tip → typ rule...
-    assert cs.transcribe("tipec") == [("c", "I", "p", "E", "t_s")]
-    # ... while this is
-    assert cs.transcribe("tipovat") == [("t", "I", "p", "o", "v", "a", "t")]
-    # franco is rewritten...
-    assert cs.transcribe("franco") == [("f", "r", "a", "N", "k", "o")]
-    # ... but only when a full match
-    assert cs.transcribe("francouz") == [("f", "r", "a", "n", "t_s", "o_u", "s")]
-    # no voicing assimilation
-    assert cs.transcribe("tbilisi") == [("t", "b", "I", "l", "I", "s", "I")]
-    # no diphthong
-    assert cs.transcribe("používat") == [("p", "o", "u", "Z", "i:", "v", "a", "t")]
-    # no hiatus
-    assert cs.transcribe("využívat") == [("v", "I", "u", "Z", "i:", "v", "a", "t")]
-    # preventing deduplication
-    assert cs.transcribe("odděl") == [("o", "d", "J\\", "E", "l")]
-    assert cs.transcribe("odtáhni") == [("o", "t", "t", "a:", "h\\", "J", "I")]
+    assert cs.transcribe(orth) == phon
 
 
-def test_separate_tokens():
-    assert cs._separate_tokens(["foo", "-bar", "baz?"], set()) == (
-        [None, None, "baz?"],
-        ["foo", "-bar"],
-    )
-    # a "-" is inserted in the list of tokens to transcribe at each prosodic
-    # boundary; it will be removed in the process of transcription after it has
-    # performed its role of blocking interaction between otherwise neighboring
-    # phones
-    assert cs._separate_tokens(["foo", "?", "bar"], {"?"}) == (
-        [None, "?", None],
-        ["foo", "-", "bar"],
-    )
+@pytest.mark.parametrize(
+    "tokens,pros_boundaries,matrix,to_transcribe",
+    [
+        (
+            ["foo", "-bar", "baz?"],
+            set(),
+            [None, None, "baz?"],
+            ["foo", "-bar"],
+        ),
+        # a "-" is inserted in the list of tokens to transcribe at each prosodic
+        # boundary; it will be removed in the process of transcription after it
+        # has performed its role of blocking interaction between otherwise
+        # neighboring phones
+        (
+            ["foo", "?", "bar"],
+            {"?"},
+            [None, "?", None],
+            ["foo", "-", "bar"],
+        ),
+    ],
+)
+def test_separate_tokens(tokens, pros_boundaries, matrix, to_transcribe):
+    assert cs._separate_tokens(tokens, pros_boundaries) == (matrix, to_transcribe)
 
 
-def test_prosodic_boundaries():
-    # no prosodic boundaries → voicing assimilation occurs (notice "d" before
-    # "?" because of following "b")
-    assert cs.transcribe("máš hlad ? Bibi též") == [
-        ("m", "a:", "Z"),
-        ("h\\", "l", "a", "d"),
-        "?",
-        ("b", "I", "b", "I"),
-        ("t", "E:", "S"),
-    ]
-    # ? is a prosodic boundary → voicing assimilation is stopped (notice "t"
-    # before "?")
-    assert cs.transcribe("máš hlad .. Bibi též", prosodic_boundary_symbols={".."}) == [
-        ("m", "a:", "Z"),
-        ("h\\", "l", "a", "t"),
-        "..",
-        ("b", "I", "b", "I"),
-        ("t", "E:", "S"),
-    ]
+class TestProsodicBoundaries:
+    def test_no_prosodic_boundaries(self):
+        # no prosodic boundaries → voicing assimilation occurs (notice "d"
+        # before "?" because of following "b")
+        assert cs.transcribe("máš hlad ? Bibi též") == [
+            ("m", "a:", "Z"),
+            ("h\\", "l", "a", "d"),
+            "?",
+            ("b", "I", "b", "I"),
+            ("t", "E:", "S"),
+        ]
+
+    def test_double_dot_as_prosodic_boundary(self):
+        # .. is a prosodic boundary → voicing assimilation is stopped (notice
+        # "t" before "..")
+        assert cs.transcribe(
+            "máš hlad .. Bibi též", prosodic_boundary_symbols={".."}
+        ) == [
+            ("m", "a:", "Z"),
+            ("h\\", "l", "a", "t"),
+            "..",
+            ("b", "I", "b", "I"),
+            ("t", "E:", "S"),
+        ]
 
 
-def test_user_added_hyphen():
-    # start of word
-    assert cs.transcribe("máš -hlad") == [("m", "a:", "S"), ("h\\", "l", "a", "t")]
-    # mid word
-    assert cs.transcribe("d-štít") == [("d", "S", "c", "i:", "t")]
-    # end of word isn't allowed
-    with pytest.raises(ValueError) as exc_info:
-        cs.transcribe("máš- hlad")
-    assert "máš-" in str(exc_info.value)
-    # and these are some ways you can have a literal hyphen
-    assert cs.transcribe("- --- ?hlad- hl?-ad etc.") == [
-        "-",
-        "---",
-        "?hlad-",
-        "hl?-ad",
-        "etc.",
-    ]
+class TestUserAddedHyphen:
+    def test_hyphen_at_start_of_word(self):
+        assert cs.transcribe("máš -hlad") == [("m", "a:", "S"), ("h\\", "l", "a", "t")]
+
+    def test_hyphen_mid_word(self):
+        assert cs.transcribe("d-štít") == [("d", "S", "c", "i:", "t")]
+
+    def test_hyphen_at_end_of_word_is_not_allowed(self):
+        with pytest.raises(ValueError) as exc_info:
+            cs.transcribe("máš- hlad")
+        assert "máš-" in str(exc_info.value)
+
+    def test_some_ways_to_have_a_literal_hyphen(self):
+        assert cs.transcribe("- --- ?hlad- hl?-ad etc.") == [
+            "-",
+            "---",
+            "?hlad-",
+            "hl?-ad",
+            "etc.",
+        ]
--- tests/test_phonetics_regressions.py
+++ tests/test_phonetics_regressions.py
@@ -1,16 +1,22 @@
+import pytest
 from pathlib import Path
 
 from corpy.phonetics import cs
+from corpy.morphodita import Tagger
 
-DIR = Path(__file__).parent
+SCRIPT_DIR = Path(__file__).parent
+TAGGER = Tagger(SCRIPT_DIR.parent / "czech-morfflex-pdt-161115.tagger")
+CASES = [
+    case.split("\t")
+    for case in (SCRIPT_DIR / "test_phonetics_regressions.tsv")
+    .read_text(encoding="utf-8")
+    .splitlines()[1:]
+]
 
 
-def test_regressions():
+@pytest.mark.parametrize("orth,phon_expected", CASES)
+def test_regressions(orth, phon_expected):
     """Test all transcriptions that worked at some point."""
-    fpath = DIR.joinpath("test_phonetics_regressions.tsv")
-    cases = fpath.read_text(encoding="utf-8").splitlines()[1:]  # pylint: disable=E1101
-    for i, case in enumerate(cases):
-        orth, phon = case.split("\t")
-        auto = cs.transcribe(orth, alphabet="cnc", hiatus=True)
-        auto = " ".join(phone for word in auto for phone in word)
-        assert auto == phon, f"auto == phon failed at {fpath}:{i+2}"
+    phon = cs.transcribe(orth, alphabet="cnc", hiatus=True, tagger=TAGGER)
+    phon = " ".join(phone for word in phon for phone in word)
+    assert phon == phon_expected
