2b898d56f8022d618f49d672773661f1a68156b8
David Lukes <dafydd.lukes@gmail.com>
2021-09-07 16:15:00 +0200

    Static analysis cleanup in morphodita.py
    Unfortunately, when I originally designed the API, I didn't really make
    it static typing friendly. This is just a stopgap to make it a little
    bit more reasonable, and to prevent type checkers from yelling at me too
    much. Maybe revisit this at some point in the future.

--- src/corpy/morphodita/tagger.py
+++ src/corpy/morphodita/tagger.py
@@ -1,10 +1,11 @@
 """An interface to MorphoDiTa taggers.
 
 """
-from typing import NamedTuple
+from typing import Union, Iterator, List, NamedTuple
 from collections.abc import Iterable
 from lazy import lazy
 from functools import lru_cache
+from pathlib import Path
 
 import ufal.morphodita as ufal
 
@@ -34,10 +35,10 @@ class Tagger:
         "strings!) of strings as the ``text`` parameter."
     )
 
-    def __init__(self, tagger_path):
+    def __init__(self, tagger_path: Union[Path, str]):
         self._tagger_path = tagger_path
         LOG.info("Loading tagger.")
-        self._tagger = ufal.Tagger.load(str(tagger_path))
+        self._tagger = ufal.Tagger.load(str(tagger_path))  # type: ignore
         if self._tagger is None:
             raise RuntimeError(f"Unable to load tagger from {tagger_path!r}!")
         self._morpho = self._tagger.getMorpho()
@@ -47,15 +48,15 @@ class Tagger:
 
     @lazy
     def _pdt_to_conll2009_converter(self):
-        return ufal.TagsetConverter_newPdtToConll2009Converter()
+        return ufal.TagsetConverter_newPdtToConll2009Converter()  # type: ignore
 
     @lazy
     def _strip_lemma_comment_converter(self):
-        return ufal.TagsetConverter_newStripLemmaCommentConverter(self._morpho)
+        return ufal.TagsetConverter_newStripLemmaCommentConverter(self._morpho)  # type: ignore
 
     @lazy
     def _strip_lemma_id_converter(self):
-        return ufal.TagsetConverter_newStripLemmaIdConverter(self._morpho)
+        return ufal.TagsetConverter_newStripLemmaIdConverter(self._morpho)  # type: ignore
 
     @lru_cache(maxsize=16)
     def _get_converter(self, convert):
@@ -77,7 +78,12 @@ class Tagger:
             ) from err
         return converter
 
-    def tag(self, text, *, sents=False, guesser=False, convert=None):
+    # NOTE: Type checkers typically won't be able to infer whether your
+    # particular call results in Iterator[Token] or Iterator[List[Token]]. Use
+    # typing.cast to tell them which it is.
+    def tag(
+        self, text, *, sents=False, guesser=False, convert=None
+    ) -> Union[Iterator[Token], Iterator[List[Token]]]:
         """Perform morphological tagging and lemmatization on text.
 
         If ``text`` is a string, sentence-split, tokenize and tag that
@@ -142,7 +148,9 @@ class Tagger:
         else:
             raise TypeError(self._TEXT_REQS)
 
-    def tag_untokenized(self, text, sents=False, guesser=False, convert=None):
+    def tag_untokenized(
+        self, text, sents=False, guesser=False, convert=None
+    ) -> Union[Iterator[Token], Iterator[List[Token]]]:
         """This is the method :meth:`tag` delegates to when `text` is a string.
         See docstring for :meth:`tag` for details about parameters.
 
@@ -152,24 +160,26 @@ class Tagger:
         tokenizer = self._tagger.newTokenizer()
         tokenizer.setText(text)
         converter = self._get_converter(convert)
-        forms = ufal.Forms()
-        tagged_lemmas = ufal.TaggedLemmas()
-        token_ranges = ufal.TokenRanges()
+        forms = ufal.Forms()  # type: ignore
+        tagged_lemmas = ufal.TaggedLemmas()  # type: ignore
+        token_ranges = ufal.TokenRanges()  # type: ignore
         yield from self._tag(
             tokenizer, sents, guesser, converter, forms, tagged_lemmas, token_ranges
         )
 
-    def tag_tokenized(self, text, sents=False, guesser=False, convert=None):
+    def tag_tokenized(
+        self, text, sents=False, guesser=False, convert=None
+    ) -> Union[Iterator[Token], Iterator[List[Token]]]:
         """This is the method :meth:`tag` delegates to when `text` is an
         iterable of iterables of strings. See docstring for :meth:`tag` for
         details about parameters.
 
         """
-        vtokenizer = ufal.Tokenizer_newVerticalTokenizer()
+        vtokenizer = ufal.Tokenizer_newVerticalTokenizer()  # type: ignore
         converter = self._get_converter(convert)
-        forms = ufal.Forms()
-        tagged_lemmas = ufal.TaggedLemmas()
-        token_ranges = ufal.TokenRanges()
+        forms = ufal.Forms()  # type: ignore
+        tagged_lemmas = ufal.TaggedLemmas()  # type: ignore
+        token_ranges = ufal.TokenRanges()  # type: ignore
         for sent in text:
             # refuse to process if sent is a string (because that would result
             # in tagging each character separately, which is nonsensical), or
@@ -189,18 +199,17 @@ class Tagger:
 
     def _tag(
         self, tokenizer, sents, guesser, converter, forms, tagged_lemmas, token_ranges
-    ):
+    ) -> Union[Iterator[Token], Iterator[List[Token]]]:
         while tokenizer.nextSentence(forms, token_ranges):
             self._tagger.tag(forms, tagged_lemmas, guesser)
-            if sents:
-                sent = []
+            sent = [] if sents else None
             for tagged_lemma, word in zip(tagged_lemmas, forms):
                 if converter is not None:
                     converter.convert(tagged_lemma)
                 token = Token(word, tagged_lemma.lemma, tagged_lemma.tag)
-                if sents:
+                if sent is not None:
                     sent.append(token)
                 else:
                     yield token
-            if sents:
+            if sent is not None:
                 yield sent
